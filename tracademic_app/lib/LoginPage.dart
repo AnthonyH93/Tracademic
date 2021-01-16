9115ce4962c35870f285c3ab1e8d5aa080f0ee0a
Chintan Mistry <csmistry@uwaterloo.ca>
2021-01-16 18:36:27 -0500

    made login screen scrollable

--- tracademic_app/lib/LoginPage.dart
+++ tracademic_app/lib/LoginPage.dart
@@ -15,76 +15,80 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Form(
         key: _formKey,
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            Text(
-              'Tracademic',
-              style: TextStyle(fontSize: 50, color: Colors.white),
-            ),
-            //Email Textfield
-            Padding(
-              padding:
-                  EdgeInsets.only(top: 70, bottom: 30, left: 10, right: 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
+            Column(
+              children: <Widget>[
+                Text(
+                  'Tracademic',
+                  style: TextStyle(fontSize: 50, color: Colors.white),
                 ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-            ),
+                //Email Textfield
+                Padding(
+                  padding:
+                      EdgeInsets.only(top: 70, bottom: 30, left: 10, right: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.all(10),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
 
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.all(10),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            //Password textfield
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: EdgeInsets.all(10),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                //Password textfield
 
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromRGBO(0, 224, 199, 100)),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                      EdgeInsets.fromLTRB(80, 15, 80, 15)),
+                Padding(
+                  padding: EdgeInsets.all(30),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(0, 224, 199, 100)),
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                          EdgeInsets.fromLTRB(80, 15, 80, 15)),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        //connect to firebase and validate user
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text('Logging in ...')));
+                      }
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
                 ),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    //connect to firebase and validate user
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Logging in ...')));
-                  }
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
+              ],
+            )
           ],
         ));
   }
