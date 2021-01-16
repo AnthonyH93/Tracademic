658ce1bcbc19dd7c0ff39285ac30f46c97b9c51b
Rushil Patel <ra8patel@uwaterloo.ca>
2021-01-16 15:52:38 -0500

    added graphs and constants dart components

---
+++ tracademic_app/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+var courses = ['ECE 252', 'ECE 224'];
+var color = [Colors.cyan, Colors.white];
+List<double> data = [
+  1.0,
+  3.0,
+  2.0,
+  3.0,
+  4.0,
+];
--- tracademic_app/lib/main.dart
+++ tracademic_app/lib/main.dart
@@ -1,26 +1,73 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:draw_graph/draw_graph.dart';
+import 'package:draw_graph/models/feature.dart';
+import 'constants.dart' as Courses;
 
-void main() => runApp(MyApp());
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  List<Feature> features = [];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tracademic',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text('Tracademic'),
+          title: Text("DrawGraph Package"),
+        ),
+        body: MyScreen(features),
+      ),
+    );
+  }
+}
+
+class MyScreen extends StatelessWidget {
+  List<Feature> features;
+  MyScreen(this.features) {
+    for (var i = 0; i < Courses.courses.length; i++) {
+      features.add(Feature(
+        title: Courses.courses[i],
+        color: Courses.color[i],
+        data: Courses.data,
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 64.0),
+          child: Text(
+            "Tasks Track",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+            ),
           ),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        LineGraph(
+          features: features,
+          size: Size(320, 300),
+          labelX: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5'],
+          labelY: ['20%', '40%', '60%', '80%', '100%'],
+          showDescription: true,
+          graphColor: Colors.white30,
         ),
-      ),
+        SizedBox(
+          height: 20,
+        )
+      ],
     );
   }
 }
