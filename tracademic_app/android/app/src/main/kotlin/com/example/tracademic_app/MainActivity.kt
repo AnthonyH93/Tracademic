0f6f1d174748643c593d2f83f02ea5d534e4435a
Chintan Mistry <csmistry@uwaterloo.ca>
2021-01-16 13:52:49 -0500

    base app

---
+++ tracademic_app/android/app/src/main/kotlin/com/example/tracademic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tracademic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tracademic_app/lib/main.dart
@@ -0,0 +1,26 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tracademic',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Tracademic'),
+          ),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
