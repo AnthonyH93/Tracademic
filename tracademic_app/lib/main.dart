719e314aa3b22b9d5e6fef47169045fcd7d7bba9
Chintan Mistry <csmistry@uwaterloo.ca>
2021-01-16 18:28:33 -0500

    separated login screen file

--- tracademic_app/lib/main.dart
+++ tracademic_app/lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'LoginPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,95 +26,5 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  //create the login page state class
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  //TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Tracademic',
-              style: TextStyle(fontSize: 50, color: Colors.white),
-            ),
-            //Email Textfield
-            Padding(
-              padding:
-                  EdgeInsets.only(top: 70, bottom: 30, left: 10, right: 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.all(10),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            //Password textfield
-
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromRGBO(0, 224, 199, 100)),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                      EdgeInsets.fromLTRB(80, 15, 80, 15)),
-                ),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    //connect to firebase and validate user
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Logging in ...')));
-                  }
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-          ],
-        ));
   }
 }
