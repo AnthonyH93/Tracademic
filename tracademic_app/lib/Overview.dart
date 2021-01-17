f416a9e94e8730b4e9b64adab1485b9b2f66f82b
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-01-17 02:11:21 -0500

    Future builder broken

--- tracademic_app/lib/Overview.dart
+++ tracademic_app/lib/Overview.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:tracademic_app/AddCourseForm.dart';
 import 'coursePage.dart';
 import 'constants.dart' as Constants;
+import 'database/DatabaseHelper.dart';
 
 class OverviewPage extends StatefulWidget {
   //create the courses page state class
@@ -10,64 +13,105 @@ class OverviewPage extends StatefulWidget {
 }
 
 class _OverviewPageState extends State<OverviewPage> {
+  final dbHelper = DatabaseHelper.instance;
+  List<String> courses;
+  List<int> grades;
+
   @override
   Widget build(BuildContext context) {
-    num average = 0;
-    for (var i = 0; i < Constants.courses_courses_grades.length; i++) {
-      average += Constants.courses_courses_grades[i];
-    }
-    average /= Constants.courses_courses_grades.length;
-    int rounded_average = average.toInt();
-    Widget averageSection = Container(
-      height: 215,
-      child: Column(children: <Widget>[
-        Text("Overall Average",
-            style: TextStyle(fontSize: 24, color: Colors.white)),
-        Text(rounded_average.toString(), style: TextStyle(fontSize: 80))
-      ]),
-    );
-    Widget coursesSection = Container(
-      child: ListView.separated(
-        itemCount: Constants.courses_courses.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => TabBarDemo(Constants.courses_courses[index], index)));
+    getAllCourses();
+
+    FutureBuilder<String>(
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+      if (snapshot.hasData) {
+        num average = 0;
+        for (var i = 0; i < courses.length; i++) {
+          average += grades[i];
+        }
+        average /= courses.length;
+        int rounded_average = average.toInt();
+        Widget averageSection = Container(
+          height: 215,
+          child: Column(children: <Widget>[
+            Text("Overall Average",
+                style: TextStyle(fontSize: 24, color: Colors.white)),
+            Text(rounded_average.toString(), style: TextStyle(fontSize: 80))
+          ]),
+        );
+        Widget coursesSection = Container(
+          child: ListView.separated(
+            itemCount: courses.length,
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              TabBarDemo(courses[index], index)));
+                },
+                child: ListTile(
+                  title: Text('${Constants.courses_courses[index]}'),
+                  trailing: Text('${Constants.courses_courses_grades[index]}'),
+                ),
+              );
             },
-            child: ListTile(
-              title: Text('${Constants.courses_courses[index]}'),
-              trailing: Text('${Constants.courses_courses_grades[index]}'),
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
+        );
+        return new Scaffold(
+          body: Column(
+            children: <Widget>[
+              averageSection,
+              Text("Courses",
+                  style: TextStyle(fontSize: 24, color: Colors.white)),
+              new Expanded(child: coursesSection)
+            ],
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Container(
+              height: 50.0,
             ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-      ),
-    );
-    return new Scaffold(
-      body: Column(
-        children: <Widget>[
-          averageSection,
-          Text("Courses", style: TextStyle(fontSize: 24, color: Colors.white)),
-          new Expanded(child: coursesSection)
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-        ),
-      ),
-      floatingActionButton: RaisedButton(
-        onPressed: () => setState(() {
-          print("Button pressed");
-          //Navigate to AddCourseForm
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => AddCourseForm()));
-        }),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Text("Add Course"),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
+          ),
+          floatingActionButton: RaisedButton(
+            onPressed: () => setState(() {
+              print("Button pressed");
+              //Navigate to AddCourseForm
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => AddCourseForm()));
+            }),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Text("Add Course"),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+        );
+      } else {
+        //Wait
+        print("here");
+        return Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [Text("Loading")],
+          ),
+        );
+      }
+    });
+  }
+
+  void getAllCourses() async {
+    int courseCount = await dbHelper.queryRowCountCourses();
+
+    if (courseCount == 0) {
+      courses = null;
+    } else {
+      List<Map<String, dynamic>> allCourses =
+          await dbHelper.queryAllRowsCourses();
+      allCourses.forEach((row) => courses.add(row[DatabaseHelper.courseName]));
+      allCourses.forEach((row) => grades.add(row[DatabaseHelper.courseGrade]));
+    }
   }
 }
