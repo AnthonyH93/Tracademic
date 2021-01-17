5fd6281b8b2041f8857a4006f77f4e3eab8dc119
Rushil Patel <ra8patel@uwaterloo.ca>
2021-01-16 23:36:33 -0500

    created basic current grades page

--- tracademic_app/lib/summaryPage.dart
+++ tracademic_app/lib/summaryPage.dart
@@ -1,37 +1,69 @@
 import 'package:flutter/material.dart';
+import 'constants.dart' as Constants;
 
 class SummaryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: ListView(
-      padding: const EdgeInsets.all(150),
+    return Column(
+      // home: ListView(
+      // padding: const EdgeInsets.all(150),
       children: <Widget>[
         Container(
-          height: 50,
-          // color: Colors.blue,
+          height: 20,
+          alignment: Alignment.centerLeft,
           child: Text(
-            'Quizzes',
+            '',
             style: TextStyle(fontSize: 30, color: Colors.black),
           ),
         ),
         Container(
-          height: 50,
-          // color: Colors.blue,
+          height: 100,
+          alignment: Alignment.center,
           child: Text(
-            'Quizzes',
-            style: TextStyle(fontSize: 30, color: Colors.black),
+            'Current Grades:',
+            style: TextStyle(
+                fontSize: 40,
+                color: Colors.blue[900],
+                fontWeight: FontWeight.bold),
           ),
         ),
         Container(
-          height: 50,
-          // color: Colors.blue,
+          height: 100,
+          alignment: Alignment.centerLeft,
           child: Text(
-            'Quizzes',
-            style: TextStyle(fontSize: 30, color: Colors.black),
+            'Labs:' + '   90',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: 100,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Quizzes:' + '    90',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: 100,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Assignments:' + '   90',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: 100,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Tests:' + '  90',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
           ),
         ),
       ],
-    ));
+    );
   }
 }
