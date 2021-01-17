4e5cefd647067be6d80b36d56d6e2b631b56f8fe
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-01-17 02:54:14 -0500

    Fixing the FutureBuilder

--- tracademic_app/lib/Overview.dart
+++ tracademic_app/lib/Overview.dart
@@ -14,95 +14,94 @@ class OverviewPage extends StatefulWidget {
 
 class _OverviewPageState extends State<OverviewPage> {
   final dbHelper = DatabaseHelper.instance;
-  List<String> courses;
-  List<int> grades;
+
+  var courses;
+  var grades;
 
   @override
-  Widget build(BuildContext context) {
-    getAllCourses();
-
-    FutureBuilder<String>(
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-      if (snapshot.hasData) {
-        num average = 0;
-        for (var i = 0; i < courses.length; i++) {
-          average += grades[i];
-        }
-        average /= courses.length;
-        int rounded_average = average.toInt();
-        Widget averageSection = Container(
-          height: 215,
-          child: Column(children: <Widget>[
-            Text("Overall Average",
-                style: TextStyle(fontSize: 24, color: Colors.white)),
-            Text(rounded_average.toString(), style: TextStyle(fontSize: 80))
-          ]),
-        );
-        Widget coursesSection = Container(
-          child: ListView.separated(
-            itemCount: courses.length,
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TabBarDemo(courses[index], index)));
-                },
-                child: ListTile(
-                  title: Text('${Constants.courses_courses[index]}'),
-                  trailing: Text('${Constants.courses_courses_grades[index]}'),
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(),
-          ),
-        );
-        return new Scaffold(
-          body: Column(
-            children: <Widget>[
-              averageSection,
-              Text("Courses",
+  Widget build(BuildContext context) => FutureBuilder(
+      future: getAllCourses(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          num average = 0;
+          for (var i = 0; i < courses.length; i++) {
+            //average += grades[i];
+          }
+          print(courses.length);
+          average /= courses.length;
+          int rounded_average = average.toInt();
+          Widget averageSection = Container(
+            height: 215,
+            child: Column(children: <Widget>[
+              Text("Overall Average",
                   style: TextStyle(fontSize: 24, color: Colors.white)),
-              new Expanded(child: coursesSection)
-            ],
-          ),
-          bottomNavigationBar: BottomAppBar(
-            child: Container(
-              height: 50.0,
+              Text(rounded_average.toString(), style: TextStyle(fontSize: 80))
+            ]),
+          );
+          Widget coursesSection = Container(
+            child: ListView.separated(
+              itemCount: courses.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                TabBarDemo(courses[index], index)));
+                  },
+                  child: ListTile(
+                    title: Text('${courses[index]}'),
+                    trailing: Text('${grades[index]}'),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
             ),
-          ),
-          floatingActionButton: RaisedButton(
-            onPressed: () => setState(() {
-              print("Button pressed");
-              //Navigate to AddCourseForm
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => AddCourseForm()));
-            }),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Text("Add Course"),
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-        );
-      } else {
-        //Wait
-        print("here");
-        return Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Text("Loading")],
-          ),
-        );
-      }
-    });
-  }
+          );
+          return new Scaffold(
+            body: Column(
+              children: <Widget>[
+                averageSection,
+                Text("Courses",
+                    style: TextStyle(fontSize: 24, color: Colors.white)),
+                new Expanded(child: coursesSection)
+              ],
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Container(
+                height: 50.0,
+              ),
+            ),
+            floatingActionButton: RaisedButton(
+              onPressed: () => setState(() {
+                print("Button pressed");
+                //Navigate to AddCourseForm
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => AddCourseForm()));
+              }),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Text("Add Course"),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+          );
+        } else {
+          //Wait
+          print("here");
+          return Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Text("Loading")],
+            ),
+          );
+        }
+      });
 
-  void getAllCourses() async {
+  Future<bool> getAllCourses() async {
     int courseCount = await dbHelper.queryRowCountCourses();
 
     if (courseCount == 0) {
@@ -110,8 +109,13 @@ class _OverviewPageState extends State<OverviewPage> {
     } else {
       List<Map<String, dynamic>> allCourses =
           await dbHelper.queryAllRowsCourses();
-      allCourses.forEach((row) => courses.add(row[DatabaseHelper.courseName]));
-      allCourses.forEach((row) => grades.add(row[DatabaseHelper.courseGrade]));
+      List<String> array = [];
+      List<int> array2 = [];
+      allCourses.forEach((row) => array.add(row[DatabaseHelper.courseName]));
+      courses = array;
+      allCourses.forEach((row) => array2.add(row[DatabaseHelper.courseGrade]));
+      grades = array2;
     }
+    return true;
   }
 }
