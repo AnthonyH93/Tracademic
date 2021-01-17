ad8a7ef648d284971b0bef2731628d938920a84f
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-01-16 20:02:21 -0500

    Adding course model

---
+++ tracademic_app/lib/models/term.dart
@@ -0,0 +1,8 @@
+//Class representing a course for the database
+class Course {
+  final int termIdentifier;
+  final String courseName;
+  final int courseGrade;
+
+  Course({this.termIdentifier, this.courseName, this.courseGrade});
+}
