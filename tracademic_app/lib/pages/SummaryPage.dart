86d0c99993b8e8798e0e18985f2aa22d3952441b
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-01-24 23:27:47 -0500

    Fixing updating database error altering course name

--- tracademic_app/lib/pages/SummaryPage.dart
+++ tracademic_app/lib/pages/SummaryPage.dart
@@ -426,7 +426,7 @@ class _SummaryOuterPageState extends State<SummaryOuterPage> {
   void updateCourseGrade(
       int courseIdToUpdate, int newGrade, String courseName) async {
     Map<String, dynamic> row = {
-      DatabaseHelper.courseIdentifier: courseIdToUpdate + 1,
+      DatabaseHelper.courseIdentifier: courseIdToUpdate,
       DatabaseHelper.courseName: courseName,
       DatabaseHelper.courseGrade: newGrade
     };
